a3f634882fd4da465a18df315c06ca713985fddb
astro <astro@LAPTOP-1P3I4PFK>
2023-02-21 21:45:13 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmReservations
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmReservations))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.lblNumberOfNights = New System.Windows.Forms.Label()
+        Me.txtNumberOfNights = New System.Windows.Forms.TextBox()
+        Me.lblCostOutput = New System.Windows.Forms.Label()
+        Me.lblCostValueDisplay = New System.Windows.Forms.Label()
+        Me.btnDisplayCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(0, -1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(404, 451)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblHeader
+        '
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.Font = New System.Drawing.Font("Broadway", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.Location = New System.Drawing.Point(425, 24)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(363, 46)
+        Me.lblHeader.TabIndex = 1
+        Me.lblHeader.Text = "Orlando AirBnB"
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Font = New System.Drawing.Font("Broadway", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCost.Location = New System.Drawing.Point(497, 87)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(218, 22)
+        Me.lblCost.TabIndex = 2
+        Me.lblCost.Text = "Only $79.00 a night"
+        '
+        'lblNumberOfNights
+        '
+        Me.lblNumberOfNights.AutoSize = True
+        Me.lblNumberOfNights.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumberOfNights.Location = New System.Drawing.Point(444, 155)
+        Me.lblNumberOfNights.Name = "lblNumberOfNights"
+        Me.lblNumberOfNights.Size = New System.Drawing.Size(185, 25)
+        Me.lblNumberOfNights.TabIndex = 3
+        Me.lblNumberOfNights.Text = "Number of Nights:"
+        '
+        'txtNumberOfNights
+        '
+        Me.txtNumberOfNights.Location = New System.Drawing.Point(658, 155)
+        Me.txtNumberOfNights.Name = "txtNumberOfNights"
+        Me.txtNumberOfNights.Size = New System.Drawing.Size(57, 22)
+        Me.txtNumberOfNights.TabIndex = 4
+        '
+        'lblCostOutput
+        '
+        Me.lblCostOutput.AutoSize = True
+        Me.lblCostOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCostOutput.Location = New System.Drawing.Point(444, 237)
+        Me.lblCostOutput.Name = "lblCostOutput"
+        Me.lblCostOutput.Size = New System.Drawing.Size(144, 25)
+        Me.lblCostOutput.TabIndex = 5
+        Me.lblCostOutput.Text = "Cost of Stay: "
+        '
+        'lblCostValueDisplay
+        '
+        Me.lblCostValueDisplay.AutoSize = True
+        Me.lblCostValueDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCostValueDisplay.Location = New System.Drawing.Point(653, 237)
+        Me.lblCostValueDisplay.Name = "lblCostValueDisplay"
+        Me.lblCostValueDisplay.Size = New System.Drawing.Size(0, 25)
+        Me.lblCostValueDisplay.TabIndex = 6
+        '
+        'btnDisplayCost
+        '
+        Me.btnDisplayCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDisplayCost.Location = New System.Drawing.Point(449, 333)
+        Me.btnDisplayCost.Name = "btnDisplayCost"
+        Me.btnDisplayCost.Size = New System.Drawing.Size(91, 37)
+        Me.btnDisplayCost.TabIndex = 7
+        Me.btnDisplayCost.Text = "Display Cost"
+        Me.btnDisplayCost.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(575, 333)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(91, 37)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(697, 333)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(91, 37)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmReservations
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkKhaki
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDisplayCost)
+        Me.Controls.Add(Me.lblCostValueDisplay)
+        Me.Controls.Add(Me.lblCostOutput)
+        Me.Controls.Add(Me.txtNumberOfNights)
+        Me.Controls.Add(Me.lblNumberOfNights)
+        Me.Controls.Add(Me.lblCost)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "frmReservations"
+        Me.Text = "AirBnB Reservations"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents lblCost As Label
+    Friend WithEvents lblNumberOfNights As Label
+    Friend WithEvents txtNumberOfNights As TextBox
+    Friend WithEvents lblCostOutput As Label
+    Friend WithEvents lblCostValueDisplay As Label
+    Friend WithEvents btnDisplayCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class frmReservations
+
+    Dim intNumNights As Integer
+    Dim strNumNights As String
+    Dim decTotalCost As Decimal
+    Const costPerNight As Decimal = 79D
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblCostValueDisplay.Text = ""
+        txtNumberOfNights.Text = ""
+        txtNumberOfNights.Focus()
+    End Sub
+
+    Private Sub btnDisplayCost_Click(sender As Object, e As EventArgs) Handles btnDisplayCost.Click
+        strNumNights = txtNumberOfNights.Text
+        intNumNights = Convert.ToInt32(strNumNights)
+        decTotalCost = intNumNights * costPerNight
+        lblCostValueDisplay.Text = decTotalCost.ToString("C")
+    End Sub
+
+    Private Sub txtNumberOfNights_TextChanged(sender As Object, e As EventArgs) Handles txtNumberOfNights.TextChanged
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+
+    Private Sub frmReservations_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblCostValueDisplay.Text = ""
+        txtNumberOfNights.Text = ""
+        txtNumberOfNights.Focus()
+    End Sub
+End Class
